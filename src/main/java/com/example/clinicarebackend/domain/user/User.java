424b4c2e5452f6d579f819f58abc474cf3687f29
Thomas Dechen <dechendev@gmail.com>
2024-05-21 20:27:47 -0300

    API login/signup

---
+++ src/main/java/com/example/clinicarebackend/domain/user/User.java
@@ -0,0 +1,19 @@
+package com.example.clinicarebackend.domain.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; // Modificado para Long
+    private String name;
+    private String email;
+    private String password;
+}
